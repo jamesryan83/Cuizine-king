0613eb3b2b7e8612f6f4bfa772a5fc8e3c6ee77b
james <james4165@hotmail.com>
2018-01-31 15:56:49 +1000

    update

--- www/js/cms/menu.js
+++ www/js/cms/menu.js
@@ -8,49 +8,62 @@ app.cms.menu = {
 
         app.storeContent.init(routeData);
 
+        this.$storeInfoEditAddress = $("#store-info-edit-address");
+
+
         // Show Edit mode
         $(".cms-menu-return").on("click", function () {
             $("#store-content-editable").removeClass("edit-mode");
             $("#store-content-preview-container").hide();
             $("#store-content-edit-container").show();
-            $(".upload-button").show();
+            $(".store-editmode-control").show();
 
             $("#preview-mode-border").hide();
             $(".cms-menu-preview").show();
             $(".cms-menu-return").hide();
-            $(".cms-menu-save").show();
         });
+
 
         // Show Preview
         $(".cms-menu-preview").on("click", function () {
             $("#store-content-editable").addClass("edit-mode");
             $("#store-content-preview-container").show();
             $("#store-content-edit-container").hide();
-            $(".upload-button").hide();
+            $(".store-editmode-control").hide();
+
+            $("#store-info-edit-address").removeClass("active");
 
             $("#preview-mode-border").show();
             $(".cms-menu-preview").hide();
             $(".cms-menu-return").show();
-            $("#cms-menu-edit").hide();
         });
 
-        $(".menu-edit-control").on("blur", function (e) {
+
+        // content editable lose focus
+        $(".store-editable-control").on("blur", function (e) {
             console.log(this.innerText)
         });
 
 
         // Edit button
         $("#cms-menu-edit").on("click", function () {
-//            $("#store-menu-edit-sidebar").animate({ right: 0 }, 200);
-
             var data = app.storeContent.getDataFromPage();
             console.log(data)
         });
 
 
-        // Sidebar close
-        $("#store-menu-edit-sidebar-close").on("click", function () {
-            $("#store-menu-edit-sidebar").animate({ right: -340 }, 200);
+        $("#store-info-edit-address-save").on("click", function () {
+            self.$storeInfoEditAddress.removeClass("active");
+        });
+
+
+        $("#store-info-edit-address-cancel").on("click", function () {
+            self.$storeInfoEditAddress.removeClass("active");
+        });
+
+
+        $("#store-info-address-edit").on("click", function () {
+            self.$storeInfoEditAddress.toggleClass("active");
         });
 
 
